863e15f8a7241c35c737343e308830c66c8ce00e
Pedro Henrique Karnoski <pedrokarnoski@gmail.com>
2024-11-13 10:55:48 -0300

    fix: Imports

--- src/http/controllers/schedules/delete.ts
+++ src/http/controllers/schedules/delete.ts
@@ -1,4 +1,4 @@
-import { FastifyReply, FastifyRequest } from 'fastify'
+import type { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
 
 import { deleteScheduleUseCase } from '@/use-cases/delete-schedule'
